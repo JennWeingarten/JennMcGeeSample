59cad5d131edd48dec2bd2bcb1d760c27667f36b
Jennifer McGee <jennifer.mcgee@bottlerocketstudios.com>
2022-06-07 08:13:23 -0500

    Playing with compose layout

--- app/src/main/java/com/example/mcgeesample/core/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mcgeesample/core/presentation/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.mcgeesample.core.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mcgeesample.theme.McGeeSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +23,64 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             McGeeSampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen(message = "Hello World")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen(message: String) {
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Box(
+            contentAlignment = Alignment.TopCenter,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Magenta)
+                .weight(3f)
+        ) {
+            Text(
+                text = message,
+                style = MaterialTheme.typography.h1,
+                textAlign = TextAlign.Center
+            )
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = "0", onValueChange = {},
+            modifier = Modifier
+                .padding(4.dp)
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(modifier = Modifier.weight(1f)) {
+            Button(
+                onClick = { },
+                modifier = Modifier
+                    .padding(4.dp)
+                    .weight(1f)
+            ) {
+                Text(text = "Increase")
+            }
+
+            Button(
+                onClick = { },
+                modifier = Modifier
+                    .padding(4.dp)
+                    .weight(1f)
+            ) {
+                Text(text = "Decrease")
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    McGeeSampleTheme {
-        Greeting("Android")
-    }
+fun PreviewMainScreen() {
+    MainScreen("Hi")
 }
